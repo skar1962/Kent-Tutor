3cf508b94ed8d45c8c97f2e80cf271a2af8a83f7
skar1962 <sudesh.patel@outlook.com>
2017-11-02 19:56:35 +0000

    Reworked contact area so it works on other browsers

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // create an anonymous function with the window.onload event.  This ensures that the window is loaded before the sendForm button is enacted.
 window.onload = function(){
+	// Check which browser we are running
+	// browserInfo();  
 	document.getElementById("sendForm").onclick = grabFormContents;
+	
 }
 
 
@@ -9,30 +12,53 @@ function grabFormContents() {
 	var myCustMessage = document.getElementById("message1");
 	var myCustEmail = document.getElementById("emailAdd");
 	var myWindow;
-    
-    /*
-	alert(myCustName.value)
-	alert(myCustMessage.value)
-	alert(myCustEmail.value)
-    */
-
-    mMailRecepient = "skar1962@googlemail.com"
-    mSubject = "Tuition query from "+myCustEmail.value
-
+ 	myMailRecepient = "sudesh.patel@outlook.com"
+  mySubject = "Tuition query from "+myCustEmail.value
 	myMailBody = "From: "+myCustName.value+"%0D%0A"+"Email Address: "+myCustEmail.value+"%0D%0A"+"Message : "+myCustMessage.value;
-	
+	// Open email window
 	openWin();
-
-	closeWin();
-	
 }
-
 	
 function openWin() {
-	myWindow=window.open('mailto:'+mMailRecepient+'?subject='+mSubject+'&body='+myMailBody, "_self", "", );
-	myWindow.document.write("<p>I replaced the current window.</p>"); 
+	window.location.href="mailto:"+myMailRecepient+"?subject="+mySubject+"&body="+myMailBody;
 }
 
-function closeWin() {
-	myWindow.close();
+function browserInfo() {
+	var txt = "";
+	txt += "Browser CodeName: " + navigator.appCodeName + "-----";
+	txt += "Browser Name: " + navigator.appName + "-----";
+	txt += "Browser Version: " + navigator.appVersion + "-----";
+	txt += "Cookies Enabled: " + navigator.cookieEnabled + "-----";
+	txt += "Browser Language: " + navigator.language + "-----";
+	txt += "Browser Online: " + navigator.onLine + "-----";
+	txt += "Platform: " + navigator.platform + "-----";
+	txt += "User-agent header: " + navigator.userAgent + "-----";
+	alert(txt);
+	
+
+	if((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1 ) 
+		{
+			alert('Opera');
+		}
+    else if(navigator.userAgent.indexOf("Chrome") != -1 )
+		{
+			alert('Chrome');
+		}
+    else if(navigator.userAgent.indexOf("Safari") != -1)
+		{
+			alert('Safari');
+		}
+    else if(navigator.userAgent.indexOf("Firefox") != -1 ) 
+		{
+			alert('Firefox');
+		}
+    else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) //IF IE > 10
+		{
+		alert('IE'); 
+		}  
+    else 
+		{
+		alert('unknown');
+		}
+	
 }
